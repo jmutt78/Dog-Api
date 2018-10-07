9e0f9f84c2cc573b71ded2b02c608007669c0648
jmutt78 <jmcintosh@docusites.com>
2018-10-07 08:03:33 -0700

    Assignmnet 1 complete

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
-$(document).ready(function(){
-  console.log('ready');
-});
+'use strict';
+
 
 //get request
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage(dogNumber) {
+  fetch('https://dog.ceo/api/breeds/image/random/${dogNumber}')
     .then(response => response.json())
     .then(responseJson =>
-      displayResults(responseJson))
+      dogDisplay(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
 //Event listerner that gets users submission//
-function inputSubmit(){
+function inputSubmit() {
+  $('#search-bar').submit(event => {
+    event.preventDefault();
+    let dogNumber = $('#search-input').val();
+    getDogImage(dogNumber);
 
-
+  });
 }
+
 
 //dispalys thumbnail and title
-function dogDisplay(){
-
+function dogDisplay(responseJson) {
+  console.log(responseJson.message);
 }
 
+
 //function handler
-function handleDogApi(){
-
-
+function handleDogApi() {
+  inputSubmit();
 }
 
 $(handleDogApi);
